7e5c868123cb4b1b318442c8b2fdcac02c71a68f
Kerem Alan <kerem.alan@ogr.sakarya.edu.tr>
2022-04-02 01:27:25 +0300

    Initial commit

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <div class="brand-logo">
+            <img src="../../../public/brand-logo.png" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">EXPLORE</a></li>
+                <li><a href="#">PROFILE</a></li>
+            </ul>
+        </nav>
+        <div class="user-action">
+            <img src="../../../public/hamburger-icon.svg" alt="">
+        </div>
+    </header>
+</template>
+<script>import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: 'Header',
+    components: {
+
+    },
+    setup() {
+
+    },
+})
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header/>
+  <main>
+    <h1>2</h1>
+  </main>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+import Header from '../components/Header/Header.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    Header,
+  },
+  setup() {
+
+  },
+})
+</script>
